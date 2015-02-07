d203d070ccd7abce504706faa4c3c5068abc27a7
SanaNasar <sanasehereen.cse@gmail.com>
2015-02-07 11:29:55 -0800

    Initial Commit

---
+++ store.js
@@ -0,0 +1,8 @@
+ListStore = {
+  getItems: function() {
+    return items;
+  },
+  loadItems: function() {},
+  addItem: function(itemDescription) {},
+  toggleCompleteness: function(itemId) {}
+};
